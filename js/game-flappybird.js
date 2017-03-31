b703de3a21bcc2e7e4e775f1ae70d0ab60848cba
mainguyen123 <mainguyen.dt3@gmail.com>
2017-03-31 21:57:23 +0700

    1. Issue #4 2. ntmai 3. Optimizing button

--- js/game-flappybird.js
+++ js/game-flappybird.js
@@ -1,6 +1,7 @@
 var myGamePiece;
 var myObstacles = [];
 var myScore;
+var g_gameState = 1; // 1:Start and 0:game over
 // To set level in game.
 var gameLevel = {   name:       new Number(),
                     score:      new Number(),
@@ -14,7 +15,7 @@ var gameLevel = {   name:       new Number(),
 
 function startGame() {
     // Create bird, background, gravity and score
-    myGamePiece = new component(50, 50, "../img/game/flappy_bird.gif", 10, 120, "image");
+    myGamePiece = new component(70, 70, "../img/game/flappy_bird_jump.gif", 10, 120, "image");
     myBackground = new component(700, 500, "../img/game/desert-level.jpg", 0, 0, "image");
     myGamePiece.gravity = 0.05;
     myScore = new component("30px", "Consolas", "black", 280, 40, "text");
@@ -25,33 +26,39 @@ var myGameArea = {
     canvas : document.createElement("canvas"),
     startup : function() {
         this.canvas.width = 700;
-        this.canvas.height = 500;
+        this.canvas.height = 300;
+        this.canvas.opacity = 0.3;
         this.canvas.style.marginLeft = "auto";
         this.canvas.style.marginRight = "auto";
         this.context = this.canvas.getContext("2d");
-        document.body.insertBefore(this.canvas, document.body.childNodes[7]);
+        document.body.insertBefore(this.canvas, document.body.childNodes[6]);
         this.frameNo = 0;
+        g_gameState = 1;
         setGameArea();
-        },
+    },
     clear : function() {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     },
     pause : function() {
         clearInterval(this.interval);
     },
-    restart : function() {
-        myObstacles = [];
+    continue: function() {
         this.interval = setInterval(updateGameArea, 20);
-    }
+    },
+    restart : function() {
+        this.frameNo = 0;
+        myObstacles = [];
+        g_gameState = 1;
+    },
 }
 
-//June add
 function component(width, height, color, x, y, type) {
     this.type = type;
     if (type == "image") {
         this.image = new Image();
         this.image.src = color;
     }
+    this.score = 0;
     this.width = width;
     this.height = height;
     this.speedX = 0;
@@ -67,7 +74,13 @@ function component(width, height, color, x, y, type) {
                 this.x, 
                 this.y,
                 this.width, this.height);
-        } else {
+        }
+        else if (this.type == "text") {
+            ctx.font = this.width + " " + this.height;
+            ctx.fillStyle = color;
+            ctx.fillText(this.text, this.x, this.y);
+        } 
+        else {
             ctx.fillStyle = color;
             ctx.fillRect(this.x, this.y, this.width, this.height);
         }
@@ -102,7 +115,7 @@ function component(width, height, color, x, y, type) {
             crash = false;
         }
         return crash;
-    }    
+    }
 }
 
 function setGameArea() {
@@ -110,8 +123,8 @@ function setGameArea() {
     myGameArea.clear();
     myBackground.newPos();
     myBackground.update();
-    myGameArea.frameNo += 1;
-    if (myGameArea.frameNo == 1 || everyinterval(250)) {
+
+    if (myGameArea.frameNo == 1 || everyinterval(150)) {
         x = myGameArea.canvas.width;
         minHeight = 20;
         maxHeight = 200;
@@ -119,8 +132,8 @@ function setGameArea() {
         minGap = 50;
         maxGap = 200;
         gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-        myObstacles.push(new component(20, height, "brown", x, 0));
-        myObstacles.push(new component(20, x - height - gap, "brown", x, height + gap));
+        myObstacles.push(new component(20, height, "green", x, 0));
+        myObstacles.push(new component(20, x - height - gap, "green", x, height + gap));
     }
     for (i = 0; i < myObstacles.length; i += 1) {
         myObstacles[i].x += -1;
@@ -136,10 +149,14 @@ function updateGameArea() {
     var x, height, gap, minHeight, maxHeight, minGap, maxGap;
     for (i = 0; i < myObstacles.length; i += 1) {
         if (myGamePiece.crashWith(myObstacles[i])) {
+            g_gameState = 0; // Die
+            document.getElementById("btnPlay").textContent = "RESTART";
             return;
-        } 
+        }
     }
+    myGameArea.frameNo += 1;
     setGameArea();
+    return;
 }
 
 function everyinterval(n) {
@@ -149,10 +166,34 @@ function everyinterval(n) {
 
 // Fly
 function move(n) {
-    myGamePiece.image.src = "../img/game/flappy_bird.gif";
+    myGamePiece.image.src = "../img/game/flappy_bird_jump.gif";
     myGamePiece.gravity = n;
 }
 
 function drop() {
     myGamePiece.image.src = "../img/game/flappy_bird_boom.gif"
 }
+
+// button
+var i = 0;
+function gameBtnPlay() {
+    if (g_gameState == 0)
+    {
+        document.getElementById("btnPlay").textContent = "PAUSE";
+        myGameArea.restart();
+    }
+    else
+    {
+        i = !i;
+        if (i == 0) { 
+            document.getElementById("btnPlay").textContent = "PLAY";
+            myGameArea.pause();
+        }
+        else{
+            document.getElementById("btnPlay").textContent = "PAUSE";
+            myGameArea.continue();
+        }
+    }
+}
+
+
